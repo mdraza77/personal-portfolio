7d051867d5acc121a2b0b901b68f03e12b6c37c3
Md Raza <mdraza8397@gmail.com>
2026-01-16 23:55:22 +0530

    favicon and title changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import "/favicon.avif";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
